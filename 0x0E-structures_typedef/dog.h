4d3eea186ad8613c0d05c39ba2775cc19d33235a
realFrankieWilson <frankuwill101@gmail.com>
2023-06-22 23:22:23 +0100

    hot fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,6 +21,7 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free(dog_t *d);
+/* void free(dog_t *d);
+ */
 
 #endif	/* dog_h */
