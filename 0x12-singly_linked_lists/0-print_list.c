e7bcfe108d4ce044744442cd785be5317565745f
realFrankieWilson <frankuwill101@gmail.com>
2023-07-05 01:39:34 +0100

    hot fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,15 @@
 
 size_t print_list(const list_t *h)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		printf("[%d] %s\n", h->len, h->str);
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
 		i++;
 		h = h->next;
