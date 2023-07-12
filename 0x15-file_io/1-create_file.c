84e9aa46cb12c418db058f343b92c52a6bff8c2a
realFrankieWilson <frankuwill101@gmail.com>
2023-07-12 02:21:28 +0100

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,8 +22,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (; text_content[i++];)
-			;
+		for (i = 0; text_content[i];)
+			i++;
 	}
 
 	/**
