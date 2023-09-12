778a22137d88d31c30c14d3b73e061d0ed94dfd8
realFrankieWilson <frankuwill101@gmail.com>
2023-09-12 22:53:55 +0100

    added more statement

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,6 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t));
 	/*free(table);*/
 	return (table);
 }
