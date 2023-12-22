3b47f6cc3967e36c0d0304d96bce196dae78dccb
realFrankieWilson <frankuwill101@gmail.com>
2023-12-22 11:27:05 +0100

    hot fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,15 +18,15 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	unsigned int i = 0;
+	size_t i = 0;
 
-	if (array)
+	if (array != NULL)
 
 		for (; i < size; i++)
 		{
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 			if (array[i] == value)
-				return (value);
+				return (i);
 		}
 	return (-1);
 }
