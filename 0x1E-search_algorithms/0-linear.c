f08136400931899b8d0c55c865ef2ece8b14fe9d
realFrankieWilson <frankuwill101@gmail.com>
2023-12-22 10:55:55 +0100

    added a new directory and some new file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * linear_search -> A function that searches for a value in an array of
+ * of integers using the Linear Search algorithm.
+ *
+ * @array: A pointer to the first element of the array to seach in.
+ * @size: The number of elements in array.
+ * @value: The value to search for.
+ *
+ * Desc:
+ *		If value is not present in array or if array is NULL, function returns
+ * -1.
+ *		Prints the value to be compared each time it compares with value
+ *		in the array.
+ *
+ * Return: Value if found, otherwise, NULL is returned.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!(array) || !(value))
+		return (-1);
+
+	while (i != size - 1)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (value);
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+/****** Libraries ******/
+#include <stdio.h>
+#include <stdlib.h>
+
+/***** Functions *******/
+int linear_search(int *, size_t, int);
+
+
+
+#endif
